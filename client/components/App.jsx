73d14543cf4eb2127a7defe71f08e6e37fd8f061
Carling Sugarman <csugarman@hmc.edu>
2016-10-27 18:07:27 -0700

    [feature] - sets up socket.io to send chat messages

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,61 @@
 import React from 'react';
+const socket = io();
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: '',
+      messageList: []
+    };
+
+    this.onMessageInput = this.onMessageInput.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.renderMessageList = this.renderMessageList.bind(this);
+  }
+
+  componentDidMount() {
+    socket.on('message', message => {
+			this.setState({ messageList: [...this.state.messageList, message] });
+		});
+  }
+
+  onMessageInput(event) {
+    this.setState({ message: event.target.value });
+  }
+
+  sendMessage(event) {
+    event.preventDefault();
+    if (this.state.message.length) {
+      socket.emit('message', this.state.message);
+      this.setState({ message: '' });
+    }
+  }
+
+  renderMessageList() {
+    return this.state.messageList.map((message, idx) => {
+			return (
+				<li key={idx}>{message}</li>
+			);
+		});
+  }
+
   render() {
-    return <h1>YO</h1>
+    return (
+      <div>
+        <ul id="messages">
+          {this.renderMessageList()}
+        </ul>
+        <form onSubmit={this.sendMessage}>
+          <input id="m"
+                 autoComplete="off"
+                 value={this.state.message}
+                 onChange={this.onMessageInput} />
+          <button>Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('./client'));
 
+io.on('connection', function(socket){
+  console.log('A user has connected.');
+
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('A user has disconnected.');
+  });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Server listening on port:', port));
+http.listen(port, () => console.log('Server listening on port:', port));
